093ba96bb87b646b5ddf953c9fd3b747ecb0c6b9
Gerald <geraldwainaina25@gmail.com>
2022-10-11 17:09:44 +0300

    view holder class

---
+++ app/src/main/java/com/gerald/newsapp/CustomViewHolder.java
@@ -0,0 +1,23 @@
+package com.gerald.newsapp;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomViewHolder extends RecyclerView.ViewHolder {
+    TextView text_title, text_source;
+    ImageView img_headline;
+    CardView cardView;
+    public CustomViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        text_title = itemView.findViewById(R.id.text_title);
+        text_source = itemView.findViewById(R.id.text_source);
+        img_headline = itemView.findViewById(R.id.img_headline);
+        cardView = itemView.findViewById(R.id.main_container);
+    }
+}
