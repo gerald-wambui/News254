c24cf1a9a168932e0bda216b876fbb237568dc43
Gerald <geraldwainaina25@gmail.com>
2022-10-11 17:09:06 +0300

    Create CustomAdapter.java

---
+++ app/src/main/java/com/gerald/newsapp/CustomAdapter.java
@@ -0,0 +1,55 @@
+package com.gerald.newsapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.gerald.newsapp.Models.NewsHeadlines;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomViewHolder> {
+    private Context context;
+    private List<NewsHeadlines> headlines;
+    private SelectListener listener;
+
+    public CustomAdapter(Context context, List<NewsHeadlines> headlines, SelectListener listener) {
+        this.context = context;
+        this.headlines = headlines;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new CustomViewHolder(LayoutInflater.from(context).inflate(R.layout.headline_list_items,parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder holder, @SuppressLint("RecyclerView") int position) {
+
+        holder.text_title.setText(headlines.get(position).getTitle());
+        holder.text_source.setText(headlines.get(position).getSource().getName());
+
+        if (headlines.get(position).getUrlToImage()!=null){
+            Picasso.get().load(headlines.get(position).getUrlToImage()).into(holder.img_headline);
+        }
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.OnNewsClicked(headlines.get(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return headlines.size();
+    }
+}
